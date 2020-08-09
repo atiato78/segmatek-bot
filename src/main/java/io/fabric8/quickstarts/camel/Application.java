e12987940961be7a05f4b762e43ab6a8555984da
atiato78 <40871759+atiato78@users.noreply.github.com>
2020-08-09 09:23:25 +0300

    Update Application.java

--- src/main/java/io/fabric8/quickstarts/camel/Application.java
+++ src/main/java/io/fabric8/quickstarts/camel/Application.java
@@ -304,7 +304,7 @@ public class Application extends SpringBootServletInitializer {
                     .log("${body}")
                     .convertBodyTo(String.class)
                     
-                    .delay(15000)
+                    .delay(5000)
 
                     .process(new Processor() {
                         public void process(Exchange exchange) throws Exception {
